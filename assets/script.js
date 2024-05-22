cef9f8202e359274386692a261c131c5b509b02d
Alien <lmllewellyn@gmail.com>
2024-05-21 23:38:08 -0400

    attempted to fix local storage object

--- assets/script.js
+++ assets/script.js
@@ -4,23 +4,28 @@ const contentInput = document.querySelector('#content');
 const submitButton = document.querySelector('#button');
 
 // Why do we need to call a function before the function?
-redirectPage();
+// redirectPage();
 
-function redirectPage() {
-    let storedUsername = localStorage.getItem('username');
-    let storedTitle = localStorage.getItem('title');
-    let storedContent = localStorage.getItem('content');
+storeData();
+
+function storeData() {
+    const data = {
+        username: usernameInput.value.trim(),
+        title: blogTitleInput.value.trim(),      
+        content: contentInput.value.trim(),
+    };
+    localStorage.setItem('data', JSON.stringify(data));
+
     // Need to figure out what you're doing here, how will this redirect to the next page
-    if (storedUsername && storedTitle && storedContent) {
-        userNameInput.value = storedUsername;
-        blogTitleInput.value = storedTitle;
-        contentInput.value = storedContent;
+    // if (storedUsername && storedTitle && storedContent) {
+        // userNameInput.value = storedUsername;
+        // blogTitleInput.value = storedTitle;
+        // contentInput.value = storedContent;
 
         // Expert assistant gave me this code but not sure if works in this case to redirect to the next page
         // window.location.href = file:///Users/lianellewellyn/bootcamp/challenges/my-blog/blog.html
-    }
+    };
 
-};
 
 submitButton.addEventListener('click', function (event) {
     event.preventDefault(); 
@@ -37,11 +42,9 @@ submitButton.addEventListener('click', function (event) {
         displayMessage('error', 'Please complete the form');
     }
 
-    localStorage.setItem('username', username);
-    localStorage.setItem('title', title);
-    localStorage.setItem('content', content);
 
-    redirectPage();
+
+    storeData();
 
 
 });
