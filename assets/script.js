d0f16def31b08d5c297932130585f7999a92cea5
Alien <lmllewellyn@gmail.com>
2024-05-22 22:24:15 -0400

    fixed local storage

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,11 @@ const submitButton = document.querySelector('#button');
 
 // const dataInputs = JSON.parse(localStorage.getItem('data')) || [];
 
+function displayMessage(type, message) {
+  msgDiv.textContent = message;
+  msgDiv.setAttribute('class', type);
+};
+
 
 function storeData() {
     const data = {
@@ -12,14 +17,9 @@ function storeData() {
         title: blogTitleInput.value.trim(),      
         content: contentInput.value.trim(),
     };
-    dataInputs.textContent = data.textContent;
-    localStorage.setItem('data', JSON.stringify(dataInputs));
+    // dataInputs.textContent = data.textContent;
+    localStorage.setItem('data', JSON.stringify(data));
 
-    // Need to figure out what you're doing here, how will this redirect to the next page
-    // if (storedUsername && storedTitle && storedContent) {
-        // userNameInput.value = storedUsername;
-        // blogTitleInput.value = storedTitle;
-        // contentInput.value = storedContent;
 
         // Expert assistant gave me this code but not sure if works in this case to redirect to the next page
         // window.location.href = file:///Users/lianellewellyn/bootcamp/challenges/my-blog/blog.html
@@ -33,13 +33,17 @@ submitButton.addEventListener('click', function (event) {
     const title = document.querySelector('#title');
     const content = document.querySelector('#content');
 
-    if (username === '') {
+    if (username === "") {
         displayMessage('error', 'Please complete the form');
-    } else if (title === '') {
+    } else if (title === "") {
         displayMessage('error', 'Please complete the form');
-    } else if (content === '') {
+    } else if (content === "") {
         displayMessage('error', 'Please complete the form');
-    }
+        localStorage.setItem('username');
+        localStorage.setItem('title');
+        localStorage.setItem('content');
+      };
+    
 
 
 
@@ -53,18 +57,18 @@ const container = document.querySelectorAll('.container-dark');
 
 let mode = 'dark';
 
-themeSwitcher.addEventListener('click', function () {
-    if (mode === 'dark') {
-      mode = 'light';
-    //   This method was given by Xpert assistant, still not functional
-      container.classList.remove('dark');
-      container.classList.add('light');
-    //   container.setAttribute('class', 'container light');
-    } else {
-      mode = 'dark';
-    //   Given by Xpert assistant
-      container.classList.remove('light');
-      container.classList.add('dark');
-    //   container.setAttribute('class', 'container dark');
-    }
-  });
+// themeSwitcher.addEventListener('click', function () {
+//     if (mode === 'dark') {
+//       mode = 'light';
+//     //   This method was given by Xpert assistant, still not functional
+//       container.classList.remove('dark');
+//       container.classList.add('light');
+//     //   container.setAttribute('class', 'container light');
+//     } else {
+//       mode = 'dark';
+//     //   Given by Xpert assistant
+//       container.classList.remove('light');
+//       container.classList.add('dark');
+//     //   container.setAttribute('class', 'container dark');
+//     }
+//   });
