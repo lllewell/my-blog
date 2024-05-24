9bde135fc0a6d9b9153a95864a1f5c5feaf5f207
Alien <lmllewellyn@gmail.com>
2024-05-24 17:47:28 -0400

    fixed toggle button on blog.html and local storage, attempting to fix pushing new inputs to local storage

--- assets/script.js
+++ assets/script.js
@@ -3,30 +3,48 @@ const blogTitleInput = document.querySelector('#title');
 const contentInput = document.querySelector('#content-box');
 const submitButton = document.querySelector('#button');
 const toggleButton = document.querySelector('#mode');
+const htmlEl = document.querySelector('html');
 
 
-const setTheme = function() {
+const setTheme = function () {
   htmlEl.dataset.theme = localStorage.getItem('theme');
 }
 
 function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute('class', type);
+  msgDiv.textContent = message;
+  msgDiv.setAttribute('class', type);
 };
 
 
-const data = JSON.parse(localStorage.getItem('data')) || [];
+
+const switchTheme = function () {
+  if (htmlEl.dataset.theme === 'dark') {
+    htmlEl.dataset.theme = 'light';
+  } else {
+    htmlEl.dataset.theme = 'dark';
+  }
+  localStorage.setItem('theme', htmlEl.dataset.theme);
+};
+
+
+const savedData = localStorage.getItem('dataInputs');
+
+const dataInputs = JSON.parse(localStorage.getItem('savedData')) || [];
 
 function storeData() {
-    const data = {
-        username: usernameInput.value.trim(),
-        title: blogTitleInput.value.trim(),
-        content: contentInput.value.trim(),
-    };
+  const dataInputs = {
+    username: usernameInput.value.trim(),
+    title: blogTitleInput.value.trim(),
+    content: contentInput.value.trim(),
+  };
 
-    data.push(data);
 
-    localStorage.setItem('data', JSON.stringify(data));
+  const arrangeData = JSON.stringify(dataInputs);
+
+
+  localStorage.setItem('arrangeData', JSON.stringify(dataInputs));
+
+  // dataInputs.push(arrangeData);
 
 
 
@@ -34,39 +52,26 @@ function storeData() {
 
 
 submitButton.addEventListener('click', function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector('#uname');
-    const title = document.querySelector('#title');
-    const content = document.querySelector('#content');
-    // Display message doesn't do anything
-    if (username === '' || title === '' || content === '') {
-        return displayMessage('error', 'Please complete the form');
-    };
+  const username = document.querySelector('#uname');
+  const title = document.querySelector('#title');
+  const content = document.querySelector('#content');
+  // Display message doesn't do anything
+  if (username === '' || title === '' || content === '') {
+    return displayMessage('error', 'Please complete the form');
+  };
 
-    localStorage.setItem('username', JSON.stringify(username));
-    localStorage.setItem('title', JSON.stringify(title));
-    localStorage.setItem('content', JSON.stringify(content));
+  // localStorage.setItem('username', JSON.stringify(username));
+  // localStorage.setItem('title', JSON.stringify(title));
+  // localStorage.setItem('content', JSON.stringify(content));
 
-    // window.location.href = '/my-blog/blog.html';
+  // window.location.href = '/my-blog/blog.html';
 
-    storeData();
+  storeData();
 
 
 
 
 });
 
-const htmlEl = document.querySelector('html');
-
-let mode = 'light';
-
-toggleButton.addEventListener('click', function() {
-    if (htmlEl.dataset.theme === 'dark') {
-      htmlEl.dataset.theme = 'light';
-    } else {
-      htmlEl.dataset.theme = 'dark';
-    };
-    
-    localStorage.setItem('theme', htmlEl.dataset.theme);
-  };
